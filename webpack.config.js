189418a7627c24049307eafcc658c5fbb83d5ebb
Yogi Patel <yogi.patel@wnco.com>
2025-03-15 13:20:43 -0400

    webpack builds to test

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,40 @@
 const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+// const CompressionPlugin = require('compression-webpack-plugin')
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production', // Enable production optimizations like minification and tree-shaking
   entry: './src/index.tsx', // Change this if your entry point is different
   output: {
-    filename: 'bundle.js', // Name for your bundled file
+    filename: '[name].[contenthash].js', // Name for your bundled file
     path: path.resolve(__dirname, 'dist'), // Output directory for the bundle
+    clean: true, // Clean the output directory before each build
   },
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}'], // Specify the paths to all of the template files in your project
   resolve: {
-    // Automatically resolve these extensions
     extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/, // Matches both .ts and .tsx files
-        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        exclude: /node_modules|dbdump/,
         use: [
           {
             loader: 'babel-loader',
             options: {
               presets: [
-                '@babel/preset-env', // Transpile modern JS features
+                '@babel/preset-env',
+                  // {
+                  //   targets: '> 0.25%, not dead',
+                  //   useBuiltIns: 'usage',
+                  //   corejs: 3,
+                  // },
                 '@babel/preset-react', // Transpile JSX if using React
+                
               ],
             },
           },
@@ -33,14 +44,16 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-        exclude: /node_modules/,
+        exclude: /node_modules|dbdump/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      minify: { collapseWhitespace: true}
     }),
+    // new BundleAnalyzerPlugin(),
   ],
   optimization: {
     splitChunks: {
@@ -48,7 +61,11 @@ module.exports = {
     },
     usedExports: true,
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+  performance: {
+    hints: "warning",
+    maxAssetSize: 250000
   },
   //   devServer: {
   //     contentBase: path.join(__dirname, 'dist'),
@@ -58,9 +75,4 @@ module.exports = {
   //       '/api': 'http://localhost:5000',
   //     },
   //   },
-  mode: 'production',
-  // Excluding dbdump from the build
-  externals: {
-    dbdump: 'dbdump'
-  }
 };
