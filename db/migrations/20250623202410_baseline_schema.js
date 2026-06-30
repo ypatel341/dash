ba4db86f4d8e01a600226bc0a7c0360ffd46b0ad
Yogi Patel <ypatel341@gmail.com>
2026-06-30 14:58:15 -0400

    creating baseline table

--- db/migrations/20250623202410_baseline_schema.js
+++ db/migrations/20250623202410_baseline_schema.js
@@ -8,39 +8,39 @@ exports.up = function (knex) {
   // It creates tables for user roles, users, monthly expenses, and monthly allocation.
   return (
     knex.schema
-      // .createTable('users_roles', function (table) {
-      //   table.uuid('id').notNullable().primary();
-      //   table.string('user_role');
-      //   table.timestamp('created_at').defaultTo(knex.fn.now());
-      //   table.timestamp('updated_at').nullable.defaultTo(knex.fn.now());
-      //   table.timestamp('deleted_at').nullable();
-      // })
-      // .createTable('user', function (table) {
-      //   table.uuid('id').notNullable().primary();
-      //   table.string('email');
-      //   table.string('first_name');
-      //   table.string('last_name');
-      //   table.string('role');
-      // })
-      // .createTable('budget_monthly_expenses', function (table) {
-      //   table.uuid('id').notNullable().primary();
-      //   table.string('person');
-      //   table.string('bucketname');
-      //   table.string('vendor');
-      //   table.integer('amount');
-      //   table.text('description');
-      //   table.timestamp('created_at').defaultTo(knex.fn.now());
-      //   table.timestamp('updated_at').nullable().defaultTo(knex.fn.now());
-      //   table.timestamp('deleted_at').nullable();
-      //   table.timestamp('expensedate').notNullable();
-      // })
-      // .createTable('budget_monthly_allocation', function (table) {
-      //   table.uuid('id').notNullable().primary();
-      //   table.string('category');
-      //   table.string('bucketname');
-      //   table.integer('amount');
-      //   table.string('household');
-      // })
+      .createTable('users_roles', function (table) {
+        table.uuid('id').notNullable().primary();
+        table.string('user_role');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').nullable.defaultTo(knex.fn.now());
+        table.timestamp('deleted_at').nullable();
+      })
+      .createTable('user', function (table) {
+        table.uuid('id').notNullable().primary();
+        table.string('email');
+        table.string('first_name');
+        table.string('last_name');
+        table.string('role');
+      })
+      .createTable('budget_monthly_expenses', function (table) {
+        table.uuid('id').notNullable().primary();
+        table.string('person');
+        table.string('bucketname');
+        table.string('vendor');
+        table.integer('amount');
+        table.text('description');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').nullable().defaultTo(knex.fn.now());
+        table.timestamp('deleted_at').nullable();
+        table.timestamp('expensedate').notNullable();
+      })
+      .createTable('budget_monthly_allocation', function (table) {
+        table.uuid('id').notNullable().primary();
+        table.string('category');
+        table.string('bucketname');
+        table.integer('amount');
+        table.string('household');
+      })
       .createTable('usersTEST', function (table) {
         table.uuid('id').notNullable().primary();
         table.string('email').notNullable().unique();
